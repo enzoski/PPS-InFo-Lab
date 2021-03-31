ae225d801116274d61d7e32317f46dc1e0a100d5
Enzo Barria <74929931+enzoski@users.noreply.github.com>
2021-03-31 17:17:35 -0300

    ext2/_rolling_test_2.py
    Agregué las pruebas de 'read_block()' y 'read_inode()'.

--- proyecto_filesystem_ext/ext2/_rolling_test_2.py
+++ proyecto_filesystem_ext/ext2/_rolling_test_2.py
@@ -5,19 +5,21 @@ import ext2
 # A continuacion se lee el area de boot, el superbloque, la tabla de descriptores de grupos y el inodo raiz.
 particion_pendrive = ext2.Ext2(r"\\.\PhysicalDrive1", 2048*512)
 
-print(particion_pendrive) #mostraremos parte del superbloque
+print(particion_pendrive) # mostraremos parte del superbloque
 
 for gd in particion_pendrive.group_descriptors[0:3]: # mostramos los 3 primeros descriptores de grupos
     print(gd)
 
-#print(particion_pendrive.group_descriptors[-3])
-#print(particion_pendrive.group_descriptors[-2])
-#print(particion_pendrive.group_descriptors[-1])
+print(particion_pendrive.group_descriptors[-1]) # mostramos el ultimo descriptor de grupo
 
 directorio_raiz = particion_pendrive.open("/")
 print(directorio_raiz) # archivos del directorio raiz
 print(directorio_raiz.show_dentries())
 print(directorio_raiz.show_inode())
+
+print(particion_pendrive.read_block(1))
+print("")
+print(particion_pendrive.read_inode(2))
 
 particion_pendrive.unmount()
 
