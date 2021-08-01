8b1c7bae6d0c00cc25d3a37a46c9fd51151d2279
Enzo Barria <74929931+enzoski@users.noreply.github.com>
2021-08-01 19:48:34 -0300

    ext2: Rolling Tests
    Agregué algunos comentarios y en el RT2 además definí la constante del tamaño de sector de disco (512 bytes).

--- proyecto_filesystem_ext/ext2/_rolling_test_2.py
+++ proyecto_filesystem_ext/ext2/_rolling_test_2.py
@@ -4,12 +4,14 @@ Testing the handling of a real Ext2 filesystem.
 
 import ext2
 
-# We open a storage device and position it at the start of a partition.
-# (the offset [sector number where the partition begins] would be given
-#  by the class MBR or GPT of haruspex [https://github.com/bconstanzo/haruspex],
-#  or seeing it in some hex/disk editor like WinHex or HxD)
+DISK_SECTOR_SIZE = 512
+
+# We open a storage device (administrator privileges are required) and position it
+# at the start of a partition. The offset (sector number where the partition begins)
+# would be given by the class MBR or GPT of haruspex [https://github.com/bconstanzo/haruspex],
+# or seeing it in some hex/disk editor like WinHex or HxD ('2048' in this case).
 # And the boot area, the superblock, the group descriptor table and the root inode are read.
-pendrive_partition = ext2.Ext2(r"\\.\PhysicalDrive1", 2048*512)
+pendrive_partition = ext2.Ext2(r"\\.\PhysicalDrive1", 2048 * DISK_SECTOR_SIZE)
 
 print(pendrive_partition) # we will show part of the superblock
 
@@ -23,7 +25,7 @@ print(root_dir) # files of root directory
 print(root_dir.show_dentries()) # the actual representation of each directory entry in the root directory
 print(root_dir.show_inode()) # the representation of the root directory inode
 
-file = pendrive_partition.open("/dir1/foo.txt")
+file = pendrive_partition.open("/dir1/foo.txt") # absolute path of the file or directory we want to open
 print(file) # internal representation of the FileHandle object
 print(file.show_inode()) # we show the inode that represents the file
 print(file.read(7))
